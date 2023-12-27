bd7c40c6fd0347cc1dacc5aa5ce9cbf2e0f57381
river861 <river861@qq.com>
2023-12-27 15:32:07 +0800

    debug

--- test/single_thread_test.cpp
+++ test/single_thread_test.cpp
@@ -40,7 +40,8 @@ int main() {
   printf("insert passed.\n");
 
   // test update
-  for (uint64_t i = TEST_NUM; i >= 1; --i) {
+  // for (uint64_t i = TEST_NUM; i >= 1; --i) {
+  for (uint64_t i = 1; i <= TEST_NUM; ++i) {
     printf("updating %lu...\n", i);
     rolex_index->update(int2key(i), i * 3);
   }
