3d8e6e506a11c737be2469176eb5737a72cfc190
river861 <river861@qq.com>
2023-12-29 02:14:12 +0800

    debug

--- src/Rolex.cpp
+++ src/Rolex.cpp
@@ -342,13 +342,16 @@ void RolexIndex::fetch_nodes(const std::vector<GlobalAddress>& leaf_addrs, std::
   std::vector<char*> raw_buffers;
   std::vector<RdmaOpRegion> rs;
 
-re_fetch:
-  raw_buffers.clear();
-  rs.clear();
   for (const auto& leaf_addr : leaf_addrs) {
     auto raw_buffer = (dsm->get_rbuf(sink)).get_leaf_buffer();
     raw_buffers.emplace_back(raw_buffer);
+  }
 
+re_fetch:
+  rs.clear();
+  for (int i = 0; i < leaf_addrs.size(); ++ i) {
+    const auto& leaf_addr = leaf_addrs[i];
+    const auto& raw_buffer = raw_buffers[i];
     RdmaOpRegion r;
     r.source     = (uint64_t)raw_buffer;
     r.dest       = leaf_addr.to_uint64();
@@ -784,13 +787,16 @@ void RolexIndex::hopscotch_fetch_nodes(const std::vector<GlobalAddress>& leaf_ad
   auto [raw_offset_l, raw_len_l, first_offset_l] = VerMng::get_offset_info(0, segment_size_l);
   assert(segment_size_l > 0 || !raw_len_l);
 
-
-re_fetch:
-  raw_buffers.clear();
-  rs.clear();
   for (const auto& leaf_addr : leaf_addrs) {
     auto raw_buffer = (dsm->get_rbuf(sink)).get_leaf_buffer();
     raw_buffers.emplace_back(raw_buffer);
+  }
+
+re_fetch:
+  rs.clear();
+  for (int i = 0; i < leaf_addrs.size(); ++ i) {
+    const auto& leaf_addr = leaf_addrs[i];
+    const auto& raw_buffer = raw_buffers[i];
     auto raw_segment_buffer_r = raw_buffer + raw_offset_r;
     auto raw_segment_buffer_l = raw_buffer + raw_offset_l;
 
