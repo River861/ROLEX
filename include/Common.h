e08d724f10ece6eb4269d3ae4ce5d558db2d0237
river861 <river861@qq.com>
2023-12-29 00:30:06 +0800

    debug

--- include/Common.h
+++ include/Common.h
@@ -142,7 +142,7 @@ constexpr uint32_t blockSize       = cachelineSize - versionSize;
 // Leaf Node
 constexpr uint32_t leafMetadataSize = versionSize + sizeof(uint64_t);
 #ifdef HOPSCOTCH_LEAF_NODE
-constexpr uint32_t leafEntrySize = versionSize + sizeof(uint8_t) + keyLen + simulatedValLen;
+constexpr uint32_t leafEntrySize = versionSize + sizeof(uint16_t) + keyLen + simulatedValLen;
 #else
 constexpr uint32_t leafEntrySize = versionSize + keyLen + simulatedValLen;
 #endif
