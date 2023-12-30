81c63e733ce2425480958e7169531bc26836a0a7
river861 <river861@qq.com>
2023-12-31 06:33:09 +0800

    test

--- include/Common.h
+++ include/Common.h
@@ -20,7 +20,7 @@
 // DEBUG-TREE
 #define HOPSCOTCH_LEAF_NODE
 #define SCATTERED_LEAF_METADATA        // !!NOTE: should be turned on together with HOPSCOTCH_LEAF_NODE
-#define SPECULATIVE_READ
+// #define SPECULATIVE_READ
 // DEBUG-SCAN (can boost small-length scan)
 // #define FINE_GRAINED_SCAN
 // #define BANDWIDTH_GREEDY_SCAN
