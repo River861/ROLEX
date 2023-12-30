ed32d4c5a96420e25b21385d31390119ef92d0a3
river861 <river861@qq.com>
2023-12-31 04:08:06 +0800

    debug

--- src/Rolex.cpp
+++ src/Rolex.cpp
@@ -219,7 +219,6 @@ void RolexIndex::insert(const Key &k, Value v, CoroPull* sink) {
   // use a leaf copy to hop since it may fail
   auto leaf_copy_buffer = (dsm->get_rbuf(sink)).get_leaf_buffer();
   memcpy(leaf_copy_buffer, (char*)leaf, define::allocationLeafSize);
-  printf("FUCK4\n");
   if (!hopscotch_insert_and_unlock((LeafNode*)leaf_copy_buffer, k, v, insert_leaf_addr, sink)) {  // return false(and remain locked) if need insert into synonym leaf
     // insert k into the synonym leaf
     GlobalAddress syn_leaf_addr = leaf->metadata.synonym_ptr;
@@ -430,7 +429,6 @@ re_read:
 #ifdef SCATTERED_LEAF_METADATA
   if (!(LeafVersionManager::decode_node_versions(raw_buffer, intermediate_buffer))) {
     read_leaf_retry[dsm->getMyThreadID()] ++;
-    printf("FUCK3\n");
     goto re_read;
   }
   MetadataManager::decode_node_metadata(intermediate_buffer, leaf_buffer);
@@ -1003,7 +1001,6 @@ re_fetch_2:
         !LeafVersionManager::decode_segment_versions(raw_segment_buffer_r, intermediate_buffers_r[i], first_offset_r, segment_size_r, first_metadata_offset_r, new_len_r, segment_node_versions_r) ||
         segment_node_versions_r != segment_node_versions_l) {  // consistency check
         read_leaf_retry[dsm->getMyThreadID()] ++;
-        printf("FUCK2\n");
         goto re_fetch_2;
       }
       bool has_metadata_l = MetadataManager::decode_segment_metadata(intermediate_buffers_l[i], (char*)&(leaves[i]->records[0]), first_metadata_offset_l, segment_size_l, metadata_l);
@@ -1035,7 +1032,6 @@ re_fetch_1:
       auto [first_metadata_offset_r, new_len_r] = MetadataManager::get_offset_info(hash_idx, segment_size_r);
       if (!LeafVersionManager::decode_segment_versions(raw_segment_buffer_r, intermediate_buffers_r[i], first_offset_r, segment_size_r, first_metadata_offset_r, new_len_r, segment_node_versions_r)) {
         read_leaf_retry[dsm->getMyThreadID()] ++;
-        printf("FUCK1\n");
         goto re_fetch_1;
       }
       auto has_metadata = MetadataManager::decode_segment_metadata(intermediate_buffers_r[i], (char*)&(leaves[i]->records[hash_idx]), first_metadata_offset_r, segment_size_r, leaves[i]->metadata);
