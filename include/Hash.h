0f3546bafa405e10dd38816183946d5a720ae397
river861 <river861@qq.com>
2023-12-30 17:46:41 +0800

    debug

--- include/Hash.h
+++ include/Hash.h
@@ -23,7 +23,7 @@ inline uint64_t get_hashed_local_lock_index(const GlobalAddress& addr) {
 inline uint64_t get_hashed_leaf_entry_index(const Key& k) {
   // return CityHash64((char *)&k, sizeof(k)) % define::leafSpanSize;
   auto p = key2int(k);
-  return CityHash64((char *)&p, sizeof(p)) % define::leafSpanSize;
+  return p % define::leafSpanSize;
 }
 
 #define HASH_TABLE_SIZE 1000000
