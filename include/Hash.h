9dff0f8f3f1850d0954b8edcede1b30c22da097d
river861 <river861@qq.com>
2023-12-30 20:10:51 +0800

    load factor bug remains

--- include/Hash.h
+++ include/Hash.h
@@ -23,9 +23,7 @@ inline uint64_t get_hashed_local_lock_index(const GlobalAddress& addr) {
 }
 
 inline uint64_t get_hashed_leaf_entry_index(const Key& k) {
-  std::hash<uint64_t> hasher;
-  return hasher(key2int(k)) % define::leafSpanSize;
-  // return CityHash64((char *)&k, sizeof(k)) % define::leafSpanSize;
+  return CityHash64((char *)&k, sizeof(k)) % define::leafSpanSize;
 }
 
 #define HASH_TABLE_SIZE 1000000
