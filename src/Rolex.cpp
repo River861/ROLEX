85f627ea531e2d45c6858ffe10d89dc02598b49b
river861 <river861@qq.com>
2024-01-25 17:17:50 +0800

    debug

--- src/Rolex.cpp
+++ src/Rolex.cpp
@@ -503,10 +503,11 @@ void RolexIndex::update(const Key &k, Value v, CoroPull* sink) {
   std::pair<bool, bool> lock_res = std::make_pair(false, false);
 
 #ifdef ENABLE_VAR_SIZE_KV
-  auto write_indirect_value = [&](Value& v){
+  Value indirect_v = v;
+  auto write_indirect_value = [=, &busy_waiting_queue](Value& v){
     // first write a new DataBlock out-of-place
     auto block_buffer = (dsm->get_rbuf(sink)).get_block_buffer();
-    auto data_block = new (block_buffer) DataBlock(v);
+    auto data_block = new (block_buffer) DataBlock(indirect_v);
     auto block_addr = dsm->alloc(define::dataBlockLen, PACKED_ADDR_ALIGN_BIT);
     dsm->write_sync_without_sink(block_buffer, block_addr, define::dataBlockLen, sink, &busy_waiting_queue);
     // change value into the DataPointer value pointing to the DataBlock
