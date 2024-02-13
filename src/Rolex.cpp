dfcadb634219cab0e6dbbdaafff290d8e4918640
Xuchuan Luo <river861@qq.com>
2024-02-13 15:37:03 +0800

    debug

--- src/Rolex.cpp
+++ src/Rolex.cpp
@@ -160,10 +160,10 @@ void RolexIndex::insert(const Key &k, Value v, CoroPull* sink) {
 
   {
   // 1. Fetching
+  auto [l, r, insert_idx] = rolex_cache->search_from_cache_for_insert(k);
 #ifdef HOPSCOTCH_LEAF_NODE
   int hash_idx = get_hashed_leaf_entry_index(k);
 #endif
-//   auto [l, r, insert_idx] = rolex_cache->search_from_cache_for_insert(k);
 //   std::vector<GlobalAddress> leaf_addrs;
 //   std::vector<LeafNode*> _;
 //   for (int i = l; i <= r; ++ i) leaf_addrs.emplace_back(get_leaf_address(i));  // without reading synonym leaves
