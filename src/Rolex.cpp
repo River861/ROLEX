ca18566d43238ed68284bacda8e84bcc808c42ce
Xuchuan Luo <river861@qq.com>
2024-02-21 20:53:46 +0800

    debug

--- src/Rolex.cpp
+++ src/Rolex.cpp
@@ -347,7 +347,7 @@ void RolexIndex::insert(const Key &k, Value v, CoroPull* sink) {
   leaf_addrs.clear();
   std::vector<LeafNode*> leaves;
   if (write_leaf) leaf_addrs.emplace_back(insert_leaf_addr), leaves.emplace_back(leaf);
-  // if (write_syn_leaf) leaf_addrs.emplace_back(syn_leaf_addrs[insert_leaf_addr]), leaves.emplace_back(syn_leaf);
+  if (write_syn_leaf) leaf_addrs.emplace_back(syn_leaf_addrs[insert_leaf_addr]), leaves.emplace_back(syn_leaf);
   // printf("FUCK 7\n");
   write_nodes_and_unlock(leaf_addrs, leaves, insert_leaf_addr, lock_buffer, sink);
   // unlock_node(insert_leaf_addr, lock_buffer, sink);
