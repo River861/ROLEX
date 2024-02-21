126de0c1bca915fae27b9fcea5b2a78c955bc8dd
Xuchuan Luo <river861@qq.com>
2024-02-21 14:53:30 +0800

    debug

--- test/single_thread_test.cpp
+++ test/single_thread_test.cpp
@@ -52,7 +52,7 @@ int main() {
   }
   for (uint64_t i = 1; i <= TEST_NUM; ++i) {
     auto res = rolex_index->search(int2key(i), v);
-    // std::cout << "search result:  " << (bool)res << " v: " << v << " ans: " << i * 3 << std::endl;
+    std::cout << "search result:  " << (bool)res << " v: " << v << " ans: " << i * 3 << std::endl;
     assert(!res || v == i * 3);
     // assert(res && v == i * 3);
     // assert(res && v == i * 2);
